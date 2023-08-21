32727b8844ce032d5af3183cc926e48900a6de1c
lmt06 <lia.taylor06518@gmail.com>
2023-08-21 09:23:04 -0700

    created basic html tags and have started arrays for questions for js

---
+++ script.js
@@ -0,0 +1,10 @@
+/*array of questions */
+const questions = ["Are you interested in building websites?", 
+"Are you interested in preparing for the programming class/ap test?",
+"Do you not have any interest in the previous two things? (be honest! No judgement :) ) ", 
+"Do you have any previous coding experience?",
+"What is the level of your experience?",
+"Have you taken honors programming?",
+"Have you taken ap programming?"]
+
+const answer1 = []
