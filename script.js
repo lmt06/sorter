4c7186b0edb85473b7f1a5cb4ab0100b045e775c
lmt06 <lia.taylor06518@gmail.com>
2023-08-22 09:01:58 -0700

    went in a different direction and instead of changing innerhtml and eventlisteners each time, button simply takes user to new page so questions/answers can be more easily counted towards categories

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/*array of questions, 7 QUESTIONS TOTAL */
+/** NOTES ON QUESTIONS AND ANSWERS, SCRAPPED ARRAY
+array of questions, 7 QUESTIONS TOTAL 
 const questions = ["Are you interested in building websites?", 
 "Are you interested in preparing for the programming class/ap test?",
 "Do you not have any interest in the previous two things? (be honest! No judgement :) ) ", 
@@ -7,9 +8,11 @@ const questions = ["Are you interested in building websites?",
 "Have you taken honors programming?",
 "Have you taken ap programming?"]
 
-/*array of answers for 1st answer choice */
+array of answers for 1st answer choice 
 const answer1 = ["Yes", "Yes", "Yes", "Yes", "No experience", "Yes", "Yes"]
 
 const answer2= ["No","No","No","No","Scratch","No","No"]
 
 const answer3= ["","","","","Higher level language","",""]
+**/
+
