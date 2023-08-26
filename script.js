6eae5a36aaddca436d335e77208f4d65c00df2cb
lmt06 <lia.taylor06518@gmail.com>
2023-08-26 12:49:14 -0700

    many changes made, tags fully created on all pages, ids added to button tags, and defined as variables in js, functions created to add 'scores' for how suited person is for each group based on questions

--- script.js
+++ script.js
@@ -16,3 +16,50 @@ const answer2= ["No","No","No","No","Scratch","No","No"]
 const answer3= ["","","","","Higher level language","",""]
 **/
 
+var q1a1 = document.getElementById("q1a1")
+var q1a2 = document.getElementById("q1a2")
+
+var q2a1 = document.getElementById("q2a1")
+var q2a2 = document.getElementById("q2a2")
+
+var q3a1 = document.getElementById("q3a1")
+var q3a2 = document.getElementById("q3a2")
+
+var q4a1 = document.getElementById("q4a1")
+var q4a2 = document.getElementById("q4a2")
+
+var q5a1 = document.getElementById("q5a1")
+var q5a2 = document.getElementById("q5a2")
+var q5a3 = document.getElementById("q5a3")
+
+var q6a1 = document.getElementById("q6a1")
+var q6a2 = document.getElementById("q6a2")
+
+//corresponds to the different groups
+var bPython = 0
+var iPython = 0
+var bJava = 0
+var iJava = 0
+var web = 0
+
+var advanced = 0
+
+function addbPython(){
+    bPython = bPython++
+}
+
+function addiPython(){
+    iPython = iPython++
+}
+
+function addbJava(){
+    bJava= bJava++
+}
+
+function addiJava(){
+    iJava= iJava++
+}
+
+function addWeb(){
+    web= web++
+}
